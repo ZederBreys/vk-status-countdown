d0bbec30a24f851374bcbc28f8e0d90dae362e45
ZederBreys <43097741+ZederBreys@users.noreply.github.com>
2019-09-01 16:58:20 +0300

    Теперь авторизация через token
    Теперь авторизация через token

--- time.py
+++ time.py
@@ -1,27 +1,8 @@
 import time, datetime, vk_api, os
 
-def auth_handler():
-    """ При двухфакторной аутентификации вызывается эта функция.
-    """
-
-    # Код двухфакторной аутентификации
-    key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
-    remember_device = True
-
-    return key, remember_device
-
-
 def main():
-    """ Пример обработки двухфакторной аутентификации """
-    login0 = os.environ.get('loginn')
-    pass0 = os.environ.get('passn')
-    login, password = str(login0), str(pass0)
-    vk_session = vk_api.VkApi(
-        login, password,
-        # функция для обработки двухфакторной аутентификации
-        auth_handler=auth_handler
-    )
+    token = os.environ.get('token')
+    vk_session = vk_api.VkApi(token=str(token))
 
     try:
         def countdown(stop):
@@ -32,7 +13,7 @@ def main():
                 if difference.days == 0 and count_hours == 0 and count_minutes == 0 and count_seconds == 0:
                     print("end")
                     break
-                vk_session.auth()
+                #vk_session.auth()
                 #vk_session.method("status.set",f"{str(difference.days)}:{str(count_hours)}:{str(count_minutes)}:{str(count_seconds)}")
                 vk_session.method("status.set",{"text":f"{str(difference.days)}:{str(count_hours)}:{str(count_minutes)}:{str(count_seconds)}"})
                 time.sleep(1)
