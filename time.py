dcb816d06272809ee541b55708cf897a5f4be2df
ZederBreys <43097741+ZederBreys@users.noreply.github.com>
2019-09-01 17:34:27 +0300

    обновление в 90 сек

--- time.py
+++ time.py
@@ -16,7 +16,7 @@ def main():
                 #vk_session.auth()
                 #vk_session.method("status.set",f"{str(difference.days)}:{str(count_hours)}:{str(count_minutes)}:{str(count_seconds)}")
                 vk_session.method("status.set",{"text":f"{str(difference.days)}:{str(count_hours)}:{str(count_minutes)}:{str(count_seconds)}"})
-                time.sleep(1)
+                time.sleep(90)
 
 
         end_time = datetime.datetime(2019, 11, 1, 0, 0, 0)
